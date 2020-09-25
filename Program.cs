1e6023a415b647ae7b01cb837afe849002d221d3
Dan Helgeland <dan.helgeland@gmail.com>
2020-09-25 17:59:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppRestarter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
