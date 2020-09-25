a6b3b9dcb66e067ef31b031deb3bc6fa29ba4c60
Dan Helgeland <dan.helgeland@gmail.com>
2020-09-25 21:28:33 +0200

    working

--- AppRestart.cs
+++ AppRestart.cs
@@ -6,27 +6,32 @@ namespace AppRestarter
 {
     public class Restarter
     {
-        public Timer _runTime = new Timer(8000*60*60);
+        public DateTime _runTime = new DateTime();
         private readonly Timer _timer;
-
+        public string _monitoredProcess = "notepad++";
+        public string _processExePath = @"C:\Program Files (x86)\Notepad++\Notepad++.exe";
         public Restarter()
         {
             _timer = new Timer(1000) { AutoReset = true };
-            _timer.Elapsed += timerElapsed;
+            _timer.Elapsed += TimerElapsed;
         }
 
-        private void timerElapsed(object sender, ElapsedEventArgs e)
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
         {
             //TODO: monitor selected service / kill and restart after given time, rinse repeat
-            var startTime = GetProcessStartTime("notepad++");
-            Console.WriteLine(startTime);
-            if (startTime >= _runTime)
+            var startTime = GetProcessStartTime(_monitoredProcess);
+            _runTime = startTime.AddSeconds(8);
+            Console.WriteLine($"compare to value: {startTime.CompareTo(_runTime)}");
+            if (DateTime.Now.CompareTo(_runTime) > 0)
             {
-                killProcess("notepad++");
+                killProcess(_monitoredProcess);
+                restartProcess(_processExePath);
             }
             else
             {
-                Console.WriteLine($"Process runtime left: {DateTime.Now-(startTime.AddSeconds(_runTime)) }");
+                Console.WriteLine($"App start time: {startTime }");
+                Console.WriteLine($"Time Now: {DateTime.Now }");
+                Console.WriteLine($"Shutdown time: {_runTime }");
             }
         }
 
@@ -48,19 +53,28 @@ namespace AppRestarter
         {
             Console.WriteLine(processName);
             Process[] p = Process.GetProcessesByName(processName);
-            if (p.Length <= 0) throw new Exception("Process not found");
-            return p[0].StartTime;
+            if (p.Length <= 0)
+            {
+                Console.WriteLine("Process not found");
+                return DateTime.Now;
+            }
+            //throw new Exception("Process not found");
+            else
+            {
+                return p[0].StartTime;
+            }
         }
 
         public void killProcess(string processName)
         {
-            Process[] p = Process.GetProcessesByName("notepad++");
+            Process[] p = Process.GetProcessesByName(_monitoredProcess);
+
             if (p.Length <= 0) throw new Exception("Process not found");
             p[0].Kill();
         }
-
-        public void restartProcess(string processName)
+        public void restartProcess(string processExePath)
         {
+            Process.Start(processExePath);
         }
     }
 }
